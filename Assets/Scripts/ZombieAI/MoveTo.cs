5ad2ae2e0a6ee42d9f6a530a60eb3ef10122f357
warriormaster12 <streng.alexander@outlook.com>
2023-12-09 17:28:23 +0200

    Add more animations in code

--- Assets/Scripts/ZombieAI/MoveTo.cs
+++ Assets/Scripts/ZombieAI/MoveTo.cs
@@ -6,45 +6,73 @@ using UnityEngine.AI;
 public class MoveTo : MonoBehaviour
 {
     [SerializeField] private string m_target_tag = "Player";
+    [SerializeField] private bool m_running = true;
     [SerializeField] private float m_speed = 5f;
     [SerializeField] private float m_acceptable_distance = 2f;
 
     private NavMeshAgent nav_mesh = null;
 
     private MeleeAttack melee_attack = null;
-    private Vector3 last_position = Vector3.zero; 
+    private Vector3 last_position = Vector3.zero;
 
     private GameObject target = null;
-    void Awake() {
+
+    public float GetSpeed()
+    {
+        return m_speed;
+    }
+
+    public GameObject GetTarget()
+    {
+        return target;
+    }
+
+    void Awake()
+    {
         nav_mesh = GetComponent<NavMeshAgent>();
-        if (!nav_mesh) {
+        if (!nav_mesh)
+        {
             Debug.LogWarning("Can't run MoveTo script due to missing NavMeshAgent component");
         }
         melee_attack = GetComponent<MeleeAttack>();
-        if (!melee_attack) {
+        if (!melee_attack)
+        {
             Debug.LogWarning("melee_attack == null, zombie can't hit you. Add script");
-        } else {
+        }
+        else
+        {
             melee_attack.enabled = false;
         }
         target = GameObject.FindGameObjectWithTag(m_target_tag);
-        if (!target) {
+        if (!target)
+        {
             Debug.LogWarning("Can't run MoveTo script due to target not being found by tag: " + m_target_tag);
-        } else {
+        }
+        else
+        {
             last_position = target.transform.position;
         }
     }
-    void FixedUpdate() {
-        if (!nav_mesh || !target) {
+    void FixedUpdate()
+    {
+        if (!nav_mesh || !target)
+        {
             target = GameObject.FindGameObjectWithTag(m_target_tag);
             return;
         }
-        if (last_position != target.transform.position || nav_mesh.destination != target.transform.position) {
+        if (!nav_mesh.enabled)
+        {
+            return;
+        }
+        if (last_position != target.transform.position || nav_mesh.destination != target.transform.position)
+        {
             nav_mesh.destination = target.transform.position;
-            nav_mesh.speed = m_speed;
+            nav_mesh.speed = m_running ? m_speed : m_speed / 2;
             nav_mesh.stoppingDistance = m_acceptable_distance;
             last_position = target.transform.position;
         }
-        if (melee_attack) {
+        if (melee_attack)
+        {
             melee_attack.enabled = nav_mesh.remainingDistance <= m_acceptable_distance;
         }
     }
--- Assets/Scripts/ZombieAI/ZombieController.cs
+++ Assets/Scripts/ZombieAI/ZombieController.cs
@@ -8,25 +8,59 @@ public class ZombieController : MonoBehaviour
     private HealthComponent health;
     private Animator animator;
     private NavMeshAgent nav_mesh;
+    private MoveTo move_to;
     void Awake()
     {
         health = GetComponent<HealthComponent>();
-        if (health) {
+        if (health)
+        {
             health.OnHealthDepleeted.AddListener(OnDeath);
-        }  
+        }
         animator = transform.GetChild(0).GetComponent<Animator>();
         nav_mesh = GetComponent<NavMeshAgent>();
+        move_to = GetComponent<MoveTo>();
     }
 
-    void Update() {
-        if (animator) {
-            if (nav_mesh) {
-                animator.SetFloat("speed", nav_mesh.velocity.normalized.magnitude);
+    void Start()
+    {
+        if (move_to)
+        {
+            move_to.GetTarget().GetComponent<HealthComponent>().OnHealthDepleeted.AddListener(OnPlayerDeath);
+        }
+    }
+
+    void Update()
+    {
+        if (animator)
+        {
+            if (nav_mesh)
+            {
+                if (!move_to)
+                {
+                    animator.SetFloat("speed", nav_mesh.velocity.normalized.magnitude);
+                }
+                else
+                {
+                    animator.SetFloat("speed", nav_mesh.velocity.magnitude / move_to.GetSpeed());
+                }
             }
         }
     }
 
-    void OnDeath() {
-        Destroy(gameObject);
+    void OnDeath()
+    {
+        if (!animator)
+        {
+            Destroy(gameObject);
+        }
+        nav_mesh.enabled = false;
+        gameObject.GetComponent<Collider>().enabled = false;
+        animator.SetBool("dying", true);
+    }
+
+    void OnPlayerDeath()
+    {
+        animator.SetBool("dancing", true);
+        nav_mesh.enabled = false;
     }
 }
